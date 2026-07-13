3b2ec8eaa1f7c54f3a47e009b8aca233be732b6b
jeremias <jeremiiias@hotmail.com>
2026-07-13 07:24:04 -0300

    feat: automatically generate missing Android debug keystore in wsl_run_gradle.sh

--- wsl_run_gradle.sh
+++ wsl_run_gradle.sh
@@ -8,6 +8,14 @@ export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
 export OTCLIENT_ANDROID_ABIS=arm64-v8a
 export CMAKE_BUILD_PARALLEL_LEVEL=1
 
+if [ ! -f "$HOME/.android/debug.keystore" ]; then
+    mkdir -p "$HOME/.android"
+    keytool -genkey -v -keystore "$HOME/.android/debug.keystore" \
+        -storepass android -alias androiddebugkey -keypass android \
+        -keyalg RSA -keysize 2048 -validity 10000 \
+        -dname "CN=Android Debug,O=Android,C=US"
+fi
+
 cd "$PROJECT_DIR/android"
 sed -i 's/\r$//' gradlew
 chmod +x gradlew

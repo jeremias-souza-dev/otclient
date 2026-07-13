bc24d9c98440394a96c4d13154f8502287ef80e3
Jeremias <jeremias.souza@acreditarfidc.com.br>
2026-07-13 12:34:05 -0300

    alterações

--- regen_datazip.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-cd /mnt/d/dbo/otclient
-rm -f android/app/src/main/assets/data.zip
-zip -r android/app/src/main/assets/data.zip data mods modules init.lua otclientrc.lua > /dev/null
-echo "data.zip regenerated: $(du -h android/app/src/main/assets/data.zip | cut -f1)"
